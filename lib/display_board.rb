301b12a5c0c32e482864f6c2f57ea6a44a73c775
Alicia Reeves <reevesalic@gmail.com>
2020-06-11 01:29:36 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -7,11 +7,19 @@ one_ox = [" O ", "   ", "   ", "   ", " X ", "   ", "   ", "   ", "   "]
 three = [" X ", " X ", " X ", "   ", "   ", "   ", "   ", "   ", "   "]
 
 def display_board(board)
+<<<<<<< HEAD
 puts " #{board[0]} | #{board[1]} | #{board[2]} "
 puts "-----------"
 puts " #{board[3]} | #{board[4]} | #{board[5]} "
 puts "-----------"
 puts " #{board[6]} | #{board[7]} | #{board[8]} "
+=======
+puts "#{board[0]}  | #{board[1]} | #{board[2]} "
+puts "-----------"
+puts "#{board[3]}  | #{board[4]} | #{board[5]} "
+puts "-----------"
+puts "#{board[6]}  | #{board[7]} | #{board[8]} "
+>>>>>>> e8c9e7b36c876c2633a77eae5ade0dc04649596c
 
 end
 print display_board(blank)
